419319baffaeaf10ce1bb2e0a708c3aef91fd00d
David Hasson <dhassonlocal@alys.local>
2009-09-12 16:38:15 -0700

    Added category/question model logic - aka "Business Logic"

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :questions
+
+  validates_uniqueness_of :name
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :category
+
+  def category_name=(category_name)
+    self.category = Category.find_or_create_by_name(category_name)
+  end
 end
