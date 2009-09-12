80b62a6e67f036a42b588d4f0ec515e281e59d0b
David Hasson <dhassonlocal@alys.local>
2009-09-12 15:42:30 -0700

    Ran nifty generator on category, question, survey

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :categories
+
+  map.resources :questions
+
+  map.resources :surveys
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
