f535ae0605162777d4168c6d25be43174ee757e3
David Hasson <dhassonlocal@alys.local>
2009-09-12 18:41:21 -0700

    jQuery tweaks

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Initialize jQuery?
+//google.load("jquery", "1.3.2");
+//google.load("jqueryui", "1.7.2");
