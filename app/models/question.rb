e639424a244be8dc6883c79c845a2ececf372874
David Hasson <dhassonlocal@alys.local>
2009-09-12 18:41:45 -0700

    Fixed question->responses relationship

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :category
+  has_many :responses
 
   def category_title=(category_title)
     self.category = Category.find_or_create_by_title(category_title)
