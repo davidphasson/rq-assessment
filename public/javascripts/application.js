00b119004b5a665d00872f8c5abbb4b572bdcf15
David Hasson <dhassonlocal@alys.local>
2009-09-12 23:45:08 -0700

    Added helper functions to advance tabs

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,30 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// Application-wide functions
 
-// Initialize jQuery?
-//google.load("jquery", "1.3.2");
-//google.load("jqueryui", "1.7.2");
+// Initialize jQuery UI Elements
+$(document).ready( function() {
+	$("#tabs").tabs();
+});
+
+function next_tab() {
+	var $tabs = $('#tabs').tabs();
+
+	// In case we're already on the last tab
+	if($tabs.tabs('option', 'selected') == ( $tabs.tabs('length') - 1) ) {
+		return false;
+	}
+
+	$tabs.tabs('select', $tabs.tabs('option', 'selected') + 1);
+	return false;
+}
+
+function prev_tab() {
+	var $tabs = $('#tabs').tabs();
+
+	// In case we're already on the first tab
+	if($tabs.tabs('option', 'selected') == 0) {
+		return false;
+	}
+
+	$tabs.tabs('select', $tabs.tabs('option', 'selected') - 1);
+	return false;
+}
