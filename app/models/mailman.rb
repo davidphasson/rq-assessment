04116eb08835da62f20eb7ec6fb2a78cfab5d95b
David Hasson <dhassonlocal@alys.local>
2009-09-14 14:10:20 -0700

    Added basic e-mail method

--- app/models/mailman.rb
+++ app/models/mailman.rb
@@ -1,4 +1,9 @@
 class Mailman < ActionMailer::Base
-  
+  def survey_completed(survey)
+    recipients "#{survey.first_name} #{survey.last_name} <#{survey.email}>"
+    subject "Your RI Assessment Results"
+    from "Relational Intelligence <noreply@relationalintelligence.info>"
+    body :survey => survey
+  end
 
 end
