86b8875b5520d5791b891ebc8e80e20bebb129f2
David Hasson <dph@funtaff.net>
2012-08-15 15:02:41 -0700

    Updated Sample with correct YAML Syntax

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -92,9 +92,11 @@ class SurveysController < ApplicationController
 
   # todo: move to app_config.yml
   def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == "admin" && password == "ri_test"
-    end
+		if APP_CONFIG['perform_authentication']
+    	authenticate_or_request_with_http_basic do |username, password|
+     	 username == APP_CONFIG['username'] && password == APP_CONFIG['password']
+    	end
+		end
   end
 
 
